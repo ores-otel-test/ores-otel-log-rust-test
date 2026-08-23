e47d6ee5ffe21b3937dc73bc0261a9ddefdb5f8f
Alexander Mills <alexander.d.mills@gmail.com>
2026-08-22 19:23:09 -0500

    chore: add hardened ores lint baseline

--- src/lib.rs
+++ src/lib.rs
@@ -1,2 +1,8 @@
 pub const LANGUAGE: &str = "rust";
-#[cfg(test)] mod tests { #[test] fn language() { assert_eq!(super::LANGUAGE, "rust"); } }
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn language() {
+        assert_eq!(super::LANGUAGE, "rust");
+    }
+}
